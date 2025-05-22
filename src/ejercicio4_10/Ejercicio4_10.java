e0b6ac58f8b3e566fb966c3ed0b251f682ce05d4
Alfonso <alfonsopampin10@gmail.com>
2025-05-22 22:02:07 +0200

    primer commit ejercicio 4.10

---
+++ src/ejercicio4_10/Ejercicio4_10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio4_10;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class Ejercicio4_10 {
+
+    public static void main(String[] args) {
+        System.out.println(Sumador.sumar(15f, 4f));
+    }
+}
